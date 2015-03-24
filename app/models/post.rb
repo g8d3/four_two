c199cd80ac09f1de705e0c06729bc0ab8acd9638
Juan David Pastás Rivera <juandavid1707@gmail.com>
2015-03-24 14:03:01 -0500

    Post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,13 @@ class Post < ActiveRecord::Base
   searchkick
   #Post.search('Mountain', fields: [:category, :description], where: {price: {gte: 500}}).size
   #
+=begin
+Post.search(where:{
+ or:[
+     [{category:'Mountain'},{description:'Mountain'}]
+ ]
+}).size
+Post.search(where:{ or:[ [{category:'Mountain'},{description:'Mountain'}, {OR}], [AND] ], AND }).size
+Post.search(where:{ or:[ [{category:'Mountain'},{description:'Mountain'}], [{price: {gte: 800}}] ] }).size
+=end
 end
